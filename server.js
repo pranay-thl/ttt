b87617f7ea0f7845036b09b38d3c73ffccbd02ca
Pranay Pandey <pranay.pnd@gmail.com>
2017-10-29 10:53:58 +0530

    changes to server

--- server.js
+++ server.js
@@ -3,59 +3,69 @@ const app = express()
 const http = require('http');
 app.use(express.static('public'));
 app.set('port', (process.env.PORT || 8000));
-var array=[];
-var str_arr;
-var index = {};
+var array = [];
 fs = require('fs')
 const file = fs.createWriteStream("ttt.txt");
 function load(cb) {
   http.get("http://terriblytinytales.com/test.txt", response => {
-    cb(response);
+    response.pipe(file);
+    cb();
   });
 }
-function read(response) {
-  response.pipe(file);
+function preprocessing(data, cb) {
+  var str_arr = data.toString().replace(/(\r\n|\n|\r|\.|,|\/|\?|!|;|-|\(|\)|–|@)/gm, " ").replace(/\s+/g, " ").toLowerCase().split(" ");
+  cb(str_arr);
+}
+function indexing(str_arr, cb) {
+  var index = {};
+  str_arr.forEach(function (word) {
+    if (!(index.hasOwnProperty(word))) {
+      index[word] = 0;
+    }
+    index[word]++;
+  });
+  cb(index);
+  //console.log("index");
+}
+function sort_words(index, cb) {
+  for (i in index) {
+    array.push({ key: i, value: index[i] });
+  }
+  cb();
+}
+function read() {
   fs.readFile('ttt.txt', 'utf8', function (err, data) {
     if (err) {
       return console.log(err);
     }
-    str_arr = data.toString().replace(/(\r\n|\n|\r|\.|,|\/|\?|!|;|-|\(|\)|–|@)/gm, " ").replace(/\s+/g, " ").toLowerCase().split(" ");
     //console.log(str_arr.toString());
-    str_arr.forEach(function (word) {
-      if (!(index.hasOwnProperty(word))) {
-        index[word] = 0;
-      }
-      index[word]++;
-    });
-    for(i in index)
-    {
-      array.push({key:i,value:index[i]});
-    }
-    array.sort(function(a, b){return b.value - a.value});
+    preprocessing(data, function (str_arr) {
+      indexing(str_arr, function (index) {
+        sort_words(index, function () {
+          array.sort(function (a, b) { return b.value - a.value });
+        });
+      });
+    })
     //console.log(array);
     //console.log("reading done");
   });
 }
-load(read);
-app.get('/getn',function(req,res){
-  var s=parseInt(req.query.n);
-  if(s>array.length||s<1)
-  {
+app.get('/getn', function (req, res) {
+  var s = parseInt(req.query.n);
+  if (s > array.length || s < 1) {
     res.send("error");
   }
-  else
-  {
-    var se_arr=array.slice(0,s);
+  else {
+    var se_arr = array.slice(0, s);
     res.send(JSON.stringify(se_arr));
   }
+})
+load(function(){
+  read();
 })
 app.get('/', function (req, res) {
   res.sendFile(__dirname + "/" + "index.html");
 })
-app.get('/delay', function (req, res) {
-  console.log("new req");
-  setTimeout(() => { res.send("Hello World!") }, 2000);
-})
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
